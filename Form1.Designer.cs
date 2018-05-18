0a55943f63b518faf35972444f3dca571bc8ba04
Alexander B <36190555+Necros4@users.noreply.github.com>
2018-05-18 22:43:12 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace KursOVA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(1, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(170, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Функція";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(11, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(75, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Гіпербола";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(11, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Еліпс";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(11, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Пряма";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(233, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(233, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(233, 37);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Пряма";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "a";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(214, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "b";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(339, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Без обмежень";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(339, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Без обмежень";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(339, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Без обмежень";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(38, 130);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "y = ax + b";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 165);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "y(x)";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(41, 162);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(216, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Обчислити";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(451, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Графік";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(506, 366);
+            this.chart1.TabIndex = 15;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(969, 399);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Обчислення кривих";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KursOVA
+{
+    public partial class Form1 : Form
+    {   // Змінна яка містить номер вибраної функції
+        int chk = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {// Змінюємо тексти Label при виборі цієї кнопки і вимикаємо інші
+            if (radioButton2.Checked == true)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                radioButton1.Checked = false;
+                radioButton3.Checked = false;
+                label1.Text = "Еліпс";
+                label8.Text = "х^2 / a^2 + у^2 / b^2 = 1";
+                label5.Text = "Без обмежень";
+                label6.Text = "Крім нуля";
+                label7.Text = "Крім нуля";
+                chk = 1;
+            }
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {// Змінюємо тексти Label при виборі цієї кнопки і вимикаємо інші
+            if (radioButton3.Checked == true)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                radioButton2.Checked = false;
+                radioButton1.Checked = false;
+                label1.Text = "Гіпербола";
+                label8.Text = "х^2 / a^2 - у^2 / b^2 = 1";
+                label5.Text = "Без обмежень";
+                label6.Text = "Крім нуля";
+                label7.Text = "Крім нуля";
+                chk = 2;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {// Змінюємо тексти Label при виборі цієї кнопки і вимикаємо інші
+            if (radioButton1.Checked == true)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                label1.Text = "Пряма";
+                label8.Text="y = ax + b";
+                label5.Text = "Без обмежень";
+                label6.Text = "Без обмежень";
+                label7.Text = "Без обмежень";
+                chk = 0;
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {// Встановлюємо обмеження на константи
+            if (radioButton2.Checked == true || radioButton3.Checked == true)
+            {
+                if (textBox2.Text != "") {
+                    if (Double.Parse(textBox2.Text) == 0)
+                    {
+                        textBox2.Text = "";
+                        MessageBox.Show("Ніяких нулів у дільнику!");
+                    }
+                }
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {// Встановлюємо обмеження на константи
+            if (radioButton2.Checked == true || radioButton3.Checked == true)
+            {
+                if (textBox1.Text != "") {
+                    if (Double.Parse(textBox1.Text) == 0)
+                    {
+                        textBox1.Text = "";
+                        MessageBox.Show("Ніяких нулів у дільнику!");
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "") //Перевіряємо наявність усіх даних
+            {
+                double x = Double.Parse(textBox3.Text);
+                double xmin = x - 3;
+                double xmax = x + 3;
+                int count = 6;
+                // Змінна для перевірки кореня
+                bool ok = true;
+                // Оголошуємо масиви значень для графіку
+                double[] xz = new double[count];
+                double[] yz = new double[count];
+                double a1 = Double.Parse(textBox2.Text), b1 = Double.Parse(textBox1.Text);
+                // В залежності від функції оголошуємо об'єкти різних класів
+                switch (chk)
+                {
+                    case 0:
+                        {
+                            Line a = new Line(a1,b1); //Оголошуємо лінійну функцію
+                            textBox4.Text = (a.Value(x)).ToString(); //Значення функції
+                            
+                            for (int i = 0; i < count; i++)
+                            {
+                                // Вычисляем значение X
+                                xz[i] = xmin + i;
+                                // Вычисляем значение функций в точке X
+                                yz[i] = a1 * xz[i] + b1;
+                            } 
+                            break;
+                        }
+                    case 1:
+                        {
+                            Ellipse b = new Ellipse(a1, b1); //Оголошуємо еліпс
+                            double m=b.Value(x);
+                            if (m != -989898)
+                            {
+                                textBox4.Text = (b.Value(x)).ToString();//Значення функції
+                                for (int i = 0; i < count; i++)
+                                {
+                                    // Обчислюемо значення X
+                                    xz[i] = xmin + i;
+                                    // Обчислюемо значення функції в точці X
+                                    yz[i] = Math.Pow(-((((xz[i] * xz[i]) / (a1 * a1)) - 1) * (b1 * b1)), 0.5);
+                                }
+                            }
+                            else
+                            {//Якщо корінь менше 0
+                                ok = false;
+                                MessageBox.Show("Функція в цій точці не існує");
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            Hyperbole c = new Hyperbole(a1, b1); //Оголошуємо гіперболу
+                            double m = c.Value(x);
+                            if (m != -989898)
+                            {
+                                textBox4.Text = (c.Value(x)).ToString();//Значення функції
+                                for (int i = 0; i < count; i++)
+                                {
+                                    // Обчислюемо значення X
+                                    xz[i] = xmin + i;
+                                    // Обчислюемо значення функції в точці X
+                                    yz[i] = Math.Pow(((((xz[i] * xz[i]) / (a1 * a1)) - 1) * (b1 * b1)), 0.5);
+                                }
+                            }
+                            else
+                            {//Якщо корінь менше 0
+                                ok =false;
+                                MessageBox.Show("Функція в цій точці не існує");
+                            }
+                            break;
+                        }
+                    
+                }
+                if (ok)
+                {//Задаємо ппараметри графіку
+                    chart1.ChartAreas[0].AxisX.Minimum = x-2;
+                    chart1.ChartAreas[0].AxisX.Maximum = x+2;
+                    chart1.ChartAreas[0].AxisY.Minimum = Double.Parse(textBox4.Text) - 2;
+                    chart1.ChartAreas[0].AxisY.Maximum = Double.Parse(textBox4.Text) + 2;
+                    double y = Double.Parse(textBox4.Text);
+                    chart1.ChartAreas[0].AxisY.MajorGrid.Interval = x; //Інтервал у
+                    chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Double.Parse(textBox4.Text); //Інтервал х
+                    chart1.Series[0].Points.DataBindXY(xz, yz); //Задаємо точки
+                    chart1.Series[0].Points[3].Color = Color.Red;                  
+                }
+            }
+            else MessageBox.Show("Введіть аргумент і коефіцієнти");
+        }
+
+    }
+    public abstract class Function//Загальний австрактний клас функцій
+    {
+        public abstract string Name();
+        public abstract double Value(double x);
+    }
+    public class Line : Function//Клас лінійної функції
+    {
+        public Line(double a, double b)
+        {
+            this._a = a;
+            this._b = b;
+
+        }
+
+        public override string Name()
+        {
+            return "Line";
+        }
+
+        public override double Value(double x)
+        {     
+            return _a * x + _b;
+        }
+       
+
+        //Змінні класу
+        private double _a;
+        private double _b;
+
+        
+    }
+    public class Ellipse : Function//Клас еліпсу
+    {
+        public Ellipse(double a, double b)
+        {
+            this._a = a;
+            this._b = b;
+        }
+
+        public override string Name()
+        {
+            return "Ellipse";
+        }
+
+        public override double Value(double x)
+        {//Перевіряє чи більший корінь за 0, якщо ні то повертаємо фіксоване значення
+            if (-((((x * x) / (_a * _a)) - 1) * (_b * _b)) > 0) return Math.Pow(-((((x * x) / (_a * _a)) - 1) * (_b * _b)), 0.5);
+            else return -989898;
+        }
+        //Змінні класу
+        private double _a;
+        private double _b;
+    }
+    public class Hyperbole : Function//Клас гіперболи
+    {
+        public Hyperbole(double a, double b)
+        {
+            this._a = a;
+            this._b = b;
+        }
+
+        public override string Name()
+        {
+            return "Hyperbole";
+        }
+
+        public override double Value(double x)
+        {//Перевіряє чи більший корінь за 0, якщо ні то повертаємо фіксоване значення
+            if (((((x * x) / (_a * _a)) - 1) * (_b * _b)) > 0) return Math.Pow(((((x * x) / (_a * _a)) - 1) * (_b * _b)), 0.5);
+            else return -989898;
+        }
+        //Змінні класу
+        private double _a;
+        private double _b;
+    }
+
+
+}
+
